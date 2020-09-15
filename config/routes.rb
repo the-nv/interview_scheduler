3917be0bae403a11047f69fe594762a67d131139
the-nv <n.verma9252@gmail.com>
2020-09-15 14:33:20 +0530

    configured database, created controller and model for candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
+
+  resources :candidates
+
+  root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
