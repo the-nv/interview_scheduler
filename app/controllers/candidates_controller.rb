97bedd0de49dd986be2c82006fd70474714931c9
the-nv <n.verma9252@gmail.com>
2020-09-15 14:50:10 +0530

    added new and show feature to candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,2 +1,21 @@
 class CandidatesController < ApplicationController
+    def show
+        @candidate = Candidate.find(params[:id])
+    end
+
+    def new
+
+    end
+
+    def create
+        @candidate = Candidate.new(candidate_params)
+        @candidate.save
+
+        redirect_to @candidate
+    end
+
+    private
+        def candidate_params
+            params.require(:candidate).permit([:name, :email])
+        end
 end
